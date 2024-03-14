b5118049a87edbb932e98a9a689a62e279924f25
unknown <morgane.de_trentinian@edu.devinci.fr>
2024-03-14 14:53:16 +0100

    crypto and get routes

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -18,6 +18,18 @@ export async function launchRegistry() {
   _registry.use(express.json());
   _registry.use(bodyParser.json());
 
+  let getNodeRegistryBody: GetNodeRegistryBody = { nodes: [] };
+
+  _registry.post("/registerNode", (req, res) => {
+    const { nodeId, pubKey } = req.body;
+    if (getNodeRegistryBody.nodes.some(n => n.nodeId === nodeId || n.pubKey === pubKey)) {
+      return res.send("Node already registered or public key in use.");
+    } else {
+      getNodeRegistryBody.nodes.push({ nodeId, pubKey });
+      return res.send("Node registered successfully.");
+    }
+  });
+
   _registry.get("/status", (req, res) => {
     res.send("live");
   });
